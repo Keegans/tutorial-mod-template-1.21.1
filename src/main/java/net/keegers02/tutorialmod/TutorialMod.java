a806bd759edf72deb81cae3c92a6581a5907d9be
Keegers02 <keegan.shaw@gmail.com>
2024-10-16 08:25:59 -0500

    Removed boilder plate log from onInitialize

--- src/main/java/net/keegers02/tutorialmod/TutorialMod.java
+++ src/main/java/net/keegers02/tutorialmod/TutorialMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// Comment
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 
@@ -19,6 +19,5 @@ public class TutorialMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
